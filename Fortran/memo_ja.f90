1ddab16cff7f77064f423f3950de82921bf23dfa
Angel de Vicente <angelv@iac.es>
2019-11-23 03:06:23 +0000

    Fortran version seems to be working OK, but there is a bug with gfortran?
    For some reason, when I compile with any optimization level (-O1, -O2, -O3), I
    get a segmentation fault. It works OK with Intel compiler and with -O0
    optimization with gfortran. Need to chase it.

---
+++ Fortran/memo_ja.f90
@@ -0,0 +1,120 @@
+!!$ Copyright (C) 2019 by Angel de Vicente, angel@iac.es 
+!!$    https://github.com/angel-devicente/                     
+!!$
+!!$ ******************************************************************
+!!$ * Angel de Vicente 
+!!$ *
+!!$ * A simple test function for 6D memoizatioin_ja.c
+!!$ * 
+!!$ ******************************************************************
+!!$ * TO DO: 
+!!$ *   + add proper tests 
+!!$ ******************************************************************
+
+program memo_ja
+  use SUFR_getopt
+  use memoization_ja
+  implicit none
+
+  ! poistion and value in array
+  integer :: l1,l2,l3,l4,l5,l6,rv
+  real :: lr
+  double precision :: v
+
+  ! A6D: jagged array
+  ! val: pointer to result of search in array
+  type(a6D) :: A
+  double precision, pointer :: val
+  
+  ! command line argument default values 
+  integer :: nvals=1000;
+  integer :: nsearch=1000;
+  integer :: maxc=40;
+  integer :: d=0;
+
+  !! timing
+  integer :: t1,t2,tt=0,count_rate, count_max
+  
+  ! 
+  character :: opt 
+
+  !==================================================================
+  
+  
+  do
+     opt = getopt("hdn:w:s:")
+     select case(opt)
+     case('h')
+        print*, "Usage: memo_ja [-h] [-n nvals] [-w maxc] [-s nsearch]"
+        print*, "  -h         : displays this help"
+        print*, "  -d         : debug mode (will print all insertions, info on searches and state of the array)"
+        print*, "  -n nvals   : nvals (default=1000) is the number of values to add to the array"
+        print*, "  -w maxc    : maxc (default=40) is the maximum length of each of the array dimensions"
+        print*, "  -s nsearch : nsearch (default=1000) is the number of elements to search for in the array"
+        stop
+     case('d')
+        d=1
+     case('n')
+        read (optArg,*) nvals
+     case('w')
+        read (optArg,*) maxc
+     case('s')
+        read (optArg,*) nsearch
+     case('>')
+        exit
+     case('!')
+        print*, "Invalid argument ",trim(optArg)," (EXITING)"
+     end select
+  end do
+
+  write(*,'(A)') NEW_LINE('A')//'Testing 6D memoization with Jagged Arrays'
+  write(*,'(A)') "=========================================="
+
+  write(*,'(A,I0,A,I0,A)') "Inserting ",nvals," random values in random positions of the array (max dimension value: ",maxc,")"
+  print*, ""
+  
+  do rv=1,nvals
+     call random_number(lr) ; l1 = FLOOR(lr*maxc)
+     call random_number(lr) ; l2 = FLOOR(lr*maxc)
+     call random_number(lr) ; l3 = FLOOR(lr*maxc)
+     call random_number(lr) ; l4 = FLOOR(lr*maxc)
+     call random_number(lr) ; l5 = FLOOR(lr*maxc)
+     call random_number(lr) ; l6 = FLOOR(lr*maxc)
+     call random_number(v)
+
+     if (d == 1) write(*,'(A,F9.6,A,6I2)') "Inserting val: ",v," at positions: ",l1,l2,l3,l4,l5,l6
+     call insert6D(l1,l2,l3,l4,l5,l6,v,A);
+     if (d == 1) then
+        write(*,*) "Jagged array:"
+        call print_jA6D(A)
+        print*, "" ; print*, ""
+     end if
+  end do
+   
+  write(*,'(A,I0,A,I0,A)') NEW_LINE('A')//"Checking for ",nsearch," random positions of the array (max dimension value: ",maxc,")"
+  print*, ""
+
+  do rv=1,nsearch
+     call random_number(lr) ; l1 = FLOOR(lr*maxc)
+     call random_number(lr) ; l2 = FLOOR(lr*maxc)
+     call random_number(lr) ; l3 = FLOOR(lr*maxc)
+     call random_number(lr) ; l4 = FLOOR(lr*maxc)
+     call random_number(lr) ; l5 = FLOOR(lr*maxc)
+     call random_number(lr) ; l6 = FLOOR(lr*maxc)
+     call random_number(v)
+    
+     if (d == 1) write(*,'(A,6I2)') "Checking position: ",l1,l2,l3,l4,l5,l6
+
+     call system_clock(t1, count_rate, count_max)
+     val => elem6D(l1,l2,l3,l4,l5,l6,A);
+     call system_clock(t2, count_rate, count_max)
+     tt = tt + (t2-t1)
+     
+     if (d == 1 .and. associated(val)) print*, val
+  end do
+ 
+
+  write(*,'(I0,A,I0,A,F9.6,A)') nsearch," positions searched in a 6D array of size ", maxc," in ", tt*1.0/count_rate," seconds"
+
+
+end program memo_ja
---
+++ Fortran/memoization_ja.f90
@@ -0,0 +1,477 @@
+!!$ * Copyright (C) 2019 by Angel de Vicente, angel@iac.es 
+!!$    https://github.com/angel-devicente/                     
+!!$ 
+!!$ ******************************************************************
+!!$ * Angel de Vicente 
+!!$ *
+!!$ * A simple program to perform memoization with dynamic jagged arrays
+!!$ * 
+!!$ ******************************************************************
+!!$ * TO DO: 
+!!$ *   + add proper description
+!!$ *   + add error handling to code
+!!$ ******************************************************************/
+
+module memoization_ja
+
+  type scalar
+     double precision, pointer :: d
+  end type scalar
+  
+  type a1D
+     type(scalar), dimension(:), pointer :: d
+  end type a1D
+
+  type a2D
+     type(a1D), dimension(:), pointer :: d
+  end type a2D
+
+  type a3D
+     type(a2D), dimension(:), pointer :: d
+  end type a3D
+
+  type a4D
+     type(a3D), dimension(:), pointer :: d
+  end type a4D
+  
+  type a5D
+     type(a4D), dimension(:), pointer :: d
+  end type a5D
+
+  type a6D
+     type(a5D), dimension(:), pointer :: d
+  end type a6D
+
+contains
+
+!************************************************************** 
+!
+! Inserting functions for 1D-6D memoization jagged arrays 
+!
+!***************************************************************
+  
+  subroutine insert_scalar(val, A)
+    double precision :: val
+    type(scalar) :: A
+
+    if (associated(A%d)) then
+       print*, "---> Repeated element. Ignoring: ", val
+    else
+       allocate(A%d)
+       A%d = val
+    end if
+  end subroutine insert_scalar
+
+
+  subroutine insert1D(l1, val, A)
+    integer :: l1
+    double precision :: val
+    type(a1D) :: A
+
+    integer :: d_size,i
+    type(scalar), dimension(:), pointer :: d_t
+    
+    if (associated(A%d)) then
+       if (l1 < size(A%d)) then
+          call insert_scalar(val,A%d(l1))
+       else   ! not big enough: we need to realloc and place value
+          d_size = size(A%d)
+          allocate(d_t(0:l1))
+          do i=0,d_size-1
+             d_t(i)%d => A%d(i)%d
+          end do
+          do i=d_size,l1
+             d_t(i)%d => null()
+          end do
+          deallocate(A%d)
+          A%d => d_t
+          call insert_scalar(val,A%d(l1))
+       end if
+
+    else
+       ! dimension does not exist yet: create and place value
+       allocate(A%d(0:l1))
+       do i=0,l1
+          A%d(i)%d => null()
+       end do
+       call insert_scalar(val,A%d(l1))
+    end if
+  end subroutine insert1D
+
+
+  subroutine insert2D(l1, l2, val, A)
+    integer :: l1, l2
+    double precision :: val
+    type(a2D) :: A
+    
+    integer :: d_size
+    type(a1D), dimension(:), pointer :: d_t
+    
+    if (associated(A%d)) then
+       if (l1 < size(A%d)) then
+          call insert1D(l2,val,A%d(l1))
+       else   ! not big enough: we need to realloc and place value
+          d_size = size(A%d)
+          allocate(d_t(0:l1))
+          do i=0,d_size-1
+             d_t(i)%d => A%d(i)%d
+          end do
+          do i=d_size,l1
+             d_t(i)%d => null()
+          end do
+          deallocate(A%d)
+          A%d => d_t
+          call insert1D(l2,val,A%d(l1))
+       end if
+
+    else
+       ! dimension does not exist yet: create and place value
+       allocate(A%d(0:l1))
+       do i=0,l1
+          A%d(i)%d => null()
+       end do
+       call insert1D(l2,val,A%d(l1))
+    end if
+  end subroutine insert2D
+
+
+  subroutine insert3D(l1, l2, l3, val, A)
+    integer :: l1,l2,l3
+    double precision :: val
+    type(a3D) :: A
+    
+    integer :: d_size
+    type(a2D), dimension(:), pointer :: d_t
+    
+    if (associated(A%d)) then
+       if (l1 < size(A%d)) then
+          call insert2D(l2,l3,val,A%d(l1))
+       else   ! not big enough: we need to realloc and place value
+          d_size = size(A%d)
+          allocate(d_t(0:l1))
+          do i=0,d_size-1
+             d_t(i)%d => A%d(i)%d
+          end do
+          do i=d_size,l1
+             d_t(i)%d => null()
+          end do
+          deallocate(A%d)
+          A%d => d_t
+          call insert2D(l2,l3,val,A%d(l1))
+       end if
+
+    else
+       ! dimension does not exist yet: create and place value
+       allocate(A%d(0:l1))
+       do i=0,l1
+          A%d(i)%d => null()
+       end do
+       call insert2D(l2,l3,val,A%d(l1))
+    end if
+  end subroutine insert3D
+
+
+  subroutine insert4D(l1, l2, l3, l4, val, A)
+    integer :: l1,l2,l3,l4
+    double precision :: val
+    type(a4D) :: A
+    
+    integer :: d_size
+    type(a3D), dimension(:), pointer :: d_t
+    
+    if (associated(A%d)) then
+       if (l1 < size(A%d)) then
+          call insert3D(l2,l3,l4,val,A%d(l1))
+       else   ! not big enough: we need to realloc and place value
+          d_size = size(A%d)
+          allocate(d_t(0:l1))
+          do i=0,d_size-1
+             d_t(i)%d => A%d(i)%d
+          end do
+          do i=d_size,l1
+             d_t(i)%d => null()
+          end do
+          deallocate(A%d)
+          A%d => d_t
+          call insert3D(l2,l3,l4,val,A%d(l1))
+       end if
+
+    else
+       ! dimension does not exist yet: create and place value
+       allocate(A%d(0:l1))
+       do i=0,l1
+          A%d(i)%d => null()
+       end do
+       call insert3D(l2,l3,l4,val,A%d(l1))
+    end if
+  end subroutine insert4D
+
+
+  subroutine insert5D(l1, l2, l3, l4, l5, val, A)
+    integer :: l1,l2,l3,l4,l5
+    double precision :: val
+    type(a5D) :: A
+    
+    integer :: d_size
+    type(a4D), dimension(:), pointer :: d_t
+    
+    if (associated(A%d)) then
+       if (l1 < size(A%d)) then
+          call insert4D(l2,l3,l4,l5,val,A%d(l1))
+       else   ! not big enough: we need to realloc and place value
+          d_size = size(A%d)
+          allocate(d_t(0:l1))
+          do i=0,d_size-1
+             d_t(i)%d => A%d(i)%d
+          end do
+          do i=d_size,l1
+             d_t(i)%d => null()
+          end do
+          deallocate(A%d)
+          A%d => d_t
+          call insert4D(l2,l3,l4,l5,val,A%d(l1))
+       end if
+
+    else
+       ! dimension does not exist yet: create and place value
+       allocate(A%d(0:l1))
+       do i=0,l1
+          A%d(i)%d => null()
+       end do
+       call insert4D(l2,l3,l4,l5,val,A%d(l1))
+    end if
+  end subroutine insert5D
+  
+
+  subroutine insert6D(l1, l2, l3, l4, l5, l6,val, A)
+    integer :: l1,l2,l3,l4,l5,l6
+    double precision :: val
+    type(a6D) :: A
+    
+    integer :: d_size
+    type(a5D), dimension(:), pointer :: d_t
+    
+    if (associated(A%d)) then
+       if (l1 < size(A%d)) then
+          call insert5D(l2,l3,l4,l5,l6,val,A%d(l1))
+       else   ! not big enough: we need to realloc and place value
+          d_size = size(A%d)
+          allocate(d_t(0:l1))
+          do i=0,d_size-1
+             d_t(i)%d => A%d(i)%d
+          end do
+          do i=d_size,l1
+             d_t(i)%d => null()
+          end do
+          deallocate(A%d)
+          A%d => d_t
+          call insert5D(l2,l3,l4,l5,l6,val,A%d(l1))
+       end if
+
+    else
+       ! dimension does not exist yet: create and place value
+       allocate(A%d(0:l1))
+       do i=0,l1
+          A%d(i)%d => null()
+       end do
+       call insert5D(l2,l3,l4,l5,l6,val,A%d(l1))
+    end if
+  end subroutine insert6D
+  
+
+
+  
+
+!************************************************************** 
+!
+! Basic printing functions for 1D-6D memoization jagged arrays 
+!
+!**************************************************************
+  
+  subroutine print_jA1D(A)
+    type(a1D) :: A
+    integer :: i
+    
+    print*, "-----------"
+
+    do i=0,size(A%d)-1
+       if (associated(A%d(i)%d)) write(*,'(A,I0,A,F9.6)') "A[",i,"] is: ",A%d(i)%d
+    end do
+  end subroutine print_jA1D
+
+
+  subroutine print_jA2D(A)
+    type(a2D) :: A
+    integer :: i,j
+    
+    print*, "-----------"
+
+    do i=0,size(A%d)-1
+       if (.not. associated(A%d(i)%d)) cycle
+       do j=0,size(A%d(i)%d)-1
+          if (.not. associated(A%d(i)%d(j)%d)) cycle
+          write(*,'(A,1(I0,A),I0,A,F9.6)') "A[",i,",",j,"] is: ",A%d(i)%d(j)%d
+       end do
+    end do
+  end subroutine print_jA2D
+
+
+  subroutine print_jA3D(A)
+    type(a3D) :: A
+    integer :: i,j,k
+    
+    print*, "-----------"
+
+    do i=0,size(A%d)-1
+       if (.not. associated(A%d(i)%d)) cycle
+       do j=0,size(A%d(i)%d)-1
+          if (.not. associated(A%d(i)%d(j)%d)) cycle
+          do k=0,size(A%d(i)%d(j)%d)-1
+             if (.not. associated(A%d(i)%d(j)%d(k)%d)) cycle
+             write(*,'(A,2(I0,A),I0,A,F9.6)') "A[",i,",",j,",",k,"] is: ",A%d(i)%d(j)%d(k)%d
+          end do
+       end do
+    end do
+  end subroutine print_jA3D
+
+
+  subroutine print_jA4D(A)
+    type(a4D) :: A
+    integer :: i,j,k,l
+    
+    print*, "-----------"
+
+    do i=0,size(A%d)-1
+       if (.not. associated(A%d(i)%d)) cycle
+       do j=0,size(A%d(i)%d)-1
+          if (.not. associated(A%d(i)%d(j)%d)) cycle
+          do k=0,size(A%d(i)%d(j)%d)-1
+             if (.not. associated(A%d(i)%d(j)%d(k)%d)) cycle
+             do l=0,size(A%d(i)%d(j)%d(k)%d)-1
+                if (.not. associated(A%d(i)%d(j)%d(k)%d(l)%d)) cycle
+                write(*,'(A,3(I0,A),I0,A,F9.6)') "A[",i,",",j,",",k,",",l,"] is: ",A%d(i)%d(j)%d(k)%d(l)%d
+             end do
+          end do
+       end do
+    end do
+  end subroutine print_jA4D
+
+
+  subroutine print_jA5D(A)
+    type(a5D) :: A
+    integer :: i,j,k,l,m
+    
+    print*, "-----------"
+
+    do i=0,size(A%d)-1
+       if (.not. associated(A%d(i)%d)) cycle
+       do j=0,size(A%d(i)%d)-1
+          if (.not. associated(A%d(i)%d(j)%d)) cycle
+          do k=0,size(A%d(i)%d(j)%d)-1
+             if (.not. associated(A%d(i)%d(j)%d(k)%d)) cycle
+             do l=0,size(A%d(i)%d(j)%d(k)%d)-1
+                if (.not. associated(A%d(i)%d(j)%d(k)%d(l)%d)) cycle
+                do m=0,size(A%d(i)%d(j)%d(k)%d(l)%d)-1
+                   if (.not. associated(A%d(i)%d(j)%d(k)%d(l)%d(m)%d)) cycle
+                   write(*,'(A,4(I0,A),I0,A,F9.6)') "A[",i,",",j,",",k,",",l,",",m,"] is: ",A%d(i)%d(j)%d(k)%d(l)%d(m)%d
+                end do
+             end do
+          end do
+       end do
+    end do
+  end subroutine print_jA5D
+  
+
+  subroutine print_jA6D(A)
+    type(a6D) :: A
+    integer :: i,j,k,l,m,n
+    
+    print*, "-----------"
+
+    do i=0,size(A%d)-1
+       if (.not. associated(A%d(i)%d)) cycle
+       do j=0,size(A%d(i)%d)-1
+          if (.not. associated(A%d(i)%d(j)%d)) cycle
+          do k=0,size(A%d(i)%d(j)%d)-1
+             if (.not. associated(A%d(i)%d(j)%d(k)%d)) cycle
+             do l=0,size(A%d(i)%d(j)%d(k)%d)-1
+                if (.not. associated(A%d(i)%d(j)%d(k)%d(l)%d)) cycle
+                do m=0,size(A%d(i)%d(j)%d(k)%d(l)%d)-1
+                   if (.not. associated(A%d(i)%d(j)%d(k)%d(l)%d(m)%d)) cycle
+                   do n=0,size(A%d(i)%d(j)%d(k)%d(l)%d(m)%d)-1
+                      if (.not. associated(A%d(i)%d(j)%d(k)%d(l)%d(m)%d(n)%d)) cycle
+                      write(*,'(A,5(I0,A),I0,A,F9.6)') "A[" ,i,",", j,"," , &
+                           k,"," , l,"," , m,"," , n,"] is: " , A%d(i)%d(j)%d(k)%d(l)%d(m)%d(n)%d
+                   end do
+                end do
+             end do
+          end do
+       end do
+    end do
+  end subroutine print_jA6D
+
+!************************************************************** 
+!
+! Search functions for 1D-6D memoization jagged arrays 
+!
+! Returns: NULL if element not in Jagged_Array.
+!          Pointer to value if present in Jagged_Array
+!
+!**************************************************************
+  function elem1D(l1,A)
+    integer :: l1
+    type(a1D) :: A
+    double precision, pointer :: elem1D
+
+    nullify(elem1D)
+    if (size(A%d) > l1) elem1d => A%d(l1)%d
+  end function elem1D
+
+
+  function elem2D(l1,l2,A)
+    integer :: l1,l2
+    type(a2D) :: A
+    double precision, pointer :: elem2D
+    
+    nullify(elem2D)
+    if ((size(A%d) > l1) .and. associated(A%d(l1)%d)) elem2D => elem1D(l2,A%d(l1))
+  end function elem2D
+
+  function elem3D(l1,l2,l3,A)
+    integer :: l1,l2,l3
+    type(a3D) :: A
+    double precision, pointer :: elem3D
+    
+    nullify(elem3D)
+    if ((size(A%d) > l1) .and. associated(A%d(l1)%d)) elem3D => elem2D(l2,l3,A%d(l1))
+  end function elem3D
+
+  function elem4D(l1,l2,l3,l4,A)
+    integer :: l1,l2,l3,l4
+    type(a4D) :: A
+    double precision, pointer :: elem4D
+    
+    nullify(elem4D)
+    if ((size(A%d) > l1) .and. associated(A%d(l1)%d)) elem4D => elem3D(l2,l3,l4,A%d(l1))
+  end function elem4D
+
+  function elem5D(l1,l2,l3,l4,l5,A)
+    integer :: l1,l2,l3,l4,l5
+    type(a5D) :: A
+    double precision, pointer :: elem5D
+    
+    nullify(elem5D)
+    if ((size(A%d) > l1) .and. associated(A%d(l1)%d)) elem5D => elem4D(l2,l3,l4,l5,A%d(l1))
+  end function elem5D
+
+  function elem6D(l1,l2,l3,l4,l5,l6,A)
+    integer :: l1,l2,l3,l4,l5,l6
+    type(a6D) :: A
+    double precision, pointer :: elem6D
+    
+    nullify(elem6D)
+    if ((size(A%d) > l1) .and. associated(A%d(l1)%d)) elem6D => elem5D(l2,l3,l4,l5,l6,A%d(l1))
+  end function elem6D
+  
+  
+end module memoization_ja
